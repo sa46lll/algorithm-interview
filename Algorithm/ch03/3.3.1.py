c8c27b7a850ec0381b224740ee2cae92cf2efb10
sa46lll <9812smh@naver.com>
2021-09-09 14:53:58 +0000

    Create 3.3.1.py

--- Algorithm/ch03/3.3.1.py
+++ Algorithm/ch03/3.3.1.py
@@ -1,4 +1,4 @@
-# 숫자 카드 게임
+# 숫자 카드 게임(2)
 
 # 여러 개의 숫자 카드 중에 가장 높은 숫자가 쓰인 카드 한장을 뽑는 게임
 # n * m 형태로 놓여있음
@@ -6,10 +6,12 @@
 n, m = map(int, input().split())
 
 result = 0
-
 for i in range(n):
   data = list(map(int, input().split()))
-  min_value = min(data)
-  result = max(result, min_value)
 
+  min_value = 10001
+  for j in data:
+    min_value = min(min_value, j)
+  result = max(result, min_value)
+  
 print(result)
