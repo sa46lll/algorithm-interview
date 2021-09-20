315e9245b91695e2637ed63bf480a5edca78afb7
sa46lll <9812smh@naver.com>
2021-09-20 14:22:50 +0000

    Create 5.2.py

---
+++ Algorithm/ch05/5.2.py
@@ -0,0 +1,21 @@
+# 큐
+
+# 선입선출: 나중에 온 사람일수록 나중에 들어감(공정한 자료구조라고도 함)
+from collections import deque
+
+# 큐 구현을 위해 deque 라이브러리 사용
+queue = deque()
+
+queue.append(5) # 5
+queue.append(2) # 5 2
+queue.append(3) # 5 2 3
+queue.append(7) # 5 2 3 7
+queue.popleft() # 2 3 7
+queue.append(1) # 2 3 7 1
+queue.append(4) # 2 3 7 1 4
+queue.popleft() # 3 7 1 4
+
+print(queue)
+queue.reverse()
+print(queue)
+
