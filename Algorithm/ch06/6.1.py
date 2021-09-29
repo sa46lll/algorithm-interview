1428c8f3dcbdbe8fa54e33b87c1e8632d138a991
sa46lll <9812smh@naver.com>
2021-09-29 14:30:21 +0000

    Create 6.2.py

--- Algorithm/ch06/6.1.py
+++ Algorithm/ch06/6.1.py
@@ -1,3 +1,5 @@
+# 선택 정렬 소스코드
+
 array = [7, 4, 9, 0, 3, 1, 6, 2, 4, 8]
 
 for i in range(len(array)):
@@ -5,6 +7,6 @@ for i in range(len(array)):
   for j in range(i+1, len(array)):
     if array[min_index] > array[j]: # index_j가 더 작을 때 min_index 와 교환
       min_index = j
-  array[i], array[min_index] = array[min_index], array[i]
+  array[i], array[min_index] = array[min_index], array[i] # 스와프
 
 print(array)
