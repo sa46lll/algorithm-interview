f86fc504d906825da3a62bb19bb9b7f73388a103
sa46lll <9812smh@naver.com>
2021-10-08 05:25:26 +0000

    Create sumMatrix.py

--- Programmers/level1/hide_number.py
+++ Programmers/level1/hide_number.py
@@ -1,6 +1,11 @@
+# 핸드폰 번호 가리기
+
+# replace 함수를 이용하여 문자열 [:-4] -> "*" 로 바꿔줌.
+
 def solution(phone_number):
   return phone_number.replace(phone_number[:-4], "*" * len(phone_number[:-4]), 1)
 
+# Test
 print(solution("01012341234"))
 print(solution("01033334444"))
 print(solution("027778888"))
