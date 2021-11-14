77365c3626d0b91c3364d9746b4ef0f62f259def
sa46lll <9812smh@naver.com>
2021-11-14 06:01:55 +0000

    Update id_recommendation.py

--- Programmers/level1/id_recommendation.py
+++ Programmers/level1/id_recommendation.py
@@ -2,25 +2,24 @@
 
 import re
 def solution(new_id):
-    #1단계 & 2단계 소문자 치환, 제거
+    # 여러문자 치환 (참고 - https://www.delftstack.com/ko/howto/python/python-replace-multiple-characters/)
     answer = re.sub('[^0-9a-z_.\-]+','',new_id.lower())
-
-    #3단계 . 2번 이상을 1개로 압축
     answer = re.sub('\.\.+','.',answer)
 
-    #4단계 양끝 . 제거
+    # 양끝 문자열 제거
     answer = answer.strip('.')
 
-    #5단계 빈문자열 a 추가
-    if answer =='': answer='a'
+    # 빈 문자열 예외
+    if not answer: answer='a'
 
-    #6단계 15개제외하고 문자모두제거, 우측 . 제거
+    # 15개 문자 추출, 마침표 제거
     answer = answer[:15].rstrip('.')
 
-    #7단계 길이 3 될 때까지 끝 문자 추가
-    answer+=answer[-1]*(3-len(answer))
+    if len(answer)<=2:
+      answer += answer[-1]*(3-len(answer))
     
     return answer
 
 
 print(solution("...!@BaT#*..y.abcdefghijklm"))
+print(solution("ab"))
