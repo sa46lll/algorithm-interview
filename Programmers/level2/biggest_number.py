1573367edc4497d4d4868b62dc0c647cc420a5cb
sa46lll <9812smh@naver.com>
2021-11-30 14:59:39 +0000

    Create biggest_number.py

---
+++ Programmers/level2/biggest_number.py
@@ -0,0 +1,10 @@
+# 가장 큰 수
+
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers = sorted(numbers, key=lambda x : x*3, reverse=True)
+    return ''.join(numbers)
+
+# Test
+print(solution([6, 10, 2])) # 6210
+print(solution([3, 30, 34, 5, 9])) # 9534330
