6ec3e1cdbc07a80bcd57080865e8c4dc907ac6d5
sa46lll <9812smh@naver.com>
2022-06-09 00:49:25 +0900

    Update lotto.py

--- Programmers/level1/lotto.py
+++ Programmers/level1/lotto.py
@@ -1,6 +1,7 @@
 # 2021 Dev-Matching
 # 로또의 최고 순위와 최저 순위
 
+# v1
 def solution(lottos, win_nums):
     cnt = 0
     cnt_zero = lottos.count(0)
@@ -9,6 +10,30 @@ def solution(lottos, win_nums):
             cnt += 1
 
     answer = [7-cnt-cnt_zero if cnt+cnt_zero > 1 else 6, 7-cnt if cnt > 1 else 6]
+    return answer
+
+
+# v2
+def solution2(lottos, win_nums):
+    list_ = [lotto for lotto in lottos if lotto in win_nums]
+    cnt = len(list_)
+    cnt_zero = lottos.count(0)
+
+    answer = [7-cnt-cnt_zero if cnt+cnt_zero > 1 else 6, 7-cnt if cnt > 1 else 6]
+    return answer
+
+
+# v3
+def solution3(lottos, win_nums):
+    rank = [6, 6, 5, 4, 3, 2, 1]
+
+    cnt = 0
+    cnt_zero = lottos.count(0)
+    for lotto in lottos:
+        if lotto in win_nums:
+            cnt += 1
+
+    answer = [rank[cnt+cnt_zero], rank[cnt]]
     return answer
 
 
