d5232d5465513aab2fc83a887936c14316516ede
sa46lll <9812smh@naver.com>
2022-06-09 00:14:14 +0900

    Update lotto.py

--- Programmers/level1/lotto.py
+++ Programmers/level1/lotto.py
@@ -2,15 +2,13 @@
 # 로또의 최고 순위와 최저 순위
 
 def solution(lottos, win_nums):
-    win_min = 0
+    cnt = 0
+    cnt_zero = lottos.count(0)
     for lotto in lottos:
         if lotto in win_nums:
-            win_min += 1
-    win_max = win_min + lottos.count(0)
+            cnt += 1
 
-    rank_min = 7 - win_min
-    rank_max = 7 - win_max
-    answer = [rank_max, rank_min if rank_min<7 else 6]
+    answer = [7-cnt-cnt_zero if cnt+cnt_zero > 1 else 6, 7-cnt if cnt > 1 else 6]
     return answer
 
 
@@ -18,4 +16,5 @@ def solution(lottos, win_nums):
 print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))  # [3,5]
 print(solution([0, 0, 0, 0, 0, 0], [38, 19, 20, 40, 15, 25]))  # [1,6]
 print(solution([45, 4, 35, 20, 3, 9], [20, 9, 3, 45, 4, 35]))  # [1,1]
+print(solution([1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12]))  # [6,6]
 
