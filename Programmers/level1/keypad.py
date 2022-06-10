60c34f5f887288b162a1c7c61442fdd97d5bdf75
sa46lll <9812smh@naver.com>
2022-06-10 18:04:41 +0900

    fix: keypad.py
    딕셔너리를 이용한 키패드 xy좌표 거리계산

--- Programmers/level1/keypad.py
+++ Programmers/level1/keypad.py
@@ -1,22 +1,50 @@
-# 키패드 누르기 - 미완
+# 2020 카카오 인턴십
+# 키패드 누르기
+
+def keypad_distance(currentN, nextN):  # 키패드 사이의 거리
+    keypad = {1: [0, 0], 2: [1, 0], 3: [2, 0],
+              4: [0, 1], 5: [1, 1], 6: [2, 1],
+              7: [0, 2], 8: [1, 2], 9: [2, 2],
+              "*": [0, 3], 0: [1, 3], "#": [2, 3]}
+
+    cur_x, cur_y = keypad[currentN]
+    next_x, next_y = keypad[nextN]
+    return abs(cur_x - next_x) + abs(cur_y - next_y)
+
 
 def solution(numbers, hand):
     result = ''
+    current_left, current_right = "*", "#"
+    hand = 'L' if hand == 'left' else 'R'
+
     for number in numbers:
         if number in [1, 4, 7]:
             result += 'L'
-            last_number = number
+            current_left = number
         elif number in [3, 6, 9]:
             result += 'R'
-            last_number = number
+            current_right = number
         else:
-            # return
-            if last_number: # last_number가 존재한다면..
-              
+            # 2, 5, 8, 0
+            left_distance = keypad_distance(current_left, number)
+            right_distance = keypad_distance(current_right, number)
+            if left_distance < right_distance:
+                result += 'L'
+                current_left = number
+            elif left_distance > right_distance:
+                result += 'R'
+                current_right = number
+            else:
+                result += hand
+                if hand == 'L':
+                    current_left = number
+                else:
+                    current_right = number
 
     return result
 
-# Test
-# print(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right"))
 
-print(solution([1, 4, 7, 3, 6, 9], "right"))
+# Test
+print(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], "right"))  # "LRLLLRLLRRL"
+print(solution([7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2], "left"))  # "LRLLRRLLLRR"
+print(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], "right"))  # "LLRLLRLLRL"
