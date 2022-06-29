d6b0d1c7d2d832886ae53c78a7148da7aa6f851e
sa46lll <9812smh@naver.com>
2022-06-29 11:00:00 +0000

    feat: prime_number.py

---
+++ programmers/level1/prime_number.py
@@ -0,0 +1,29 @@
+from itertools import permutations
+
+
+# 소수 찾기
+def solution(numbers):
+    answer = []
+    nums = [n for n in numbers]
+    per = []
+    for i in range(1, len(numbers)+1):
+        per += list(permutations(nums, i))
+    new_nums = [int(("").join(p)) for p in per]
+
+    for n in new_nums:
+        if n < 2:
+            continue
+        check = True
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                check = False
+                break
+        if check:
+            answer.append(n)
+
+    return len(set(answer))
+
+
+# Test
+print(solution("17"))  # 3
+print(solution("011"))  # 2
