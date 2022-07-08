8f7cf2fdbf76cda0c1c072edf486bf486ff66542
sa46lll <9812smh@naver.com>
2022-07-08 11:00:00 +0000

    feat: unfinished_participant.py

---
+++ programmers/practice/unfinished_participant.py
@@ -0,0 +1,16 @@
+# 완주하지 못한 선수
+
+def solution(participant, completion):
+    d = {}
+    for x in participant:
+        d[x] = d.get(x, 0) + 1  # get 함수 -> x가 존재하면 해당하는 값을, 없으면 0
+    for x in completion:
+        d[x] -= 1
+    dnf = [k for k, v in d.items() if v > 0]
+    answer = dnf[0]
+    return answer
+
+
+# test
+print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))  # "leo"
+print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))  # "mislav"
